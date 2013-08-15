70a165bf602e23b08c0d9e8151a42f8bc218ef96
Sander Smits <sander.smits@nelen-schuurmans.nl>
2013-08-15 14:45:08 +0200

    Upgrade code for Django 1.5

--- example/settings.py
+++ example/settings.py
@@ -11,12 +11,14 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'sqlite3'
-DATABASE_NAME = os.path.join(BASE_DIR, 'example', 'database.sqlite')
-DATABASE_USER = ''             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+DATABASES = {
+    'default': {
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'USER': '',
+        'PASSWORD': ''
+    }
+}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -37,12 +39,16 @@ USE_I18N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = os.path.join(BASE_DIR, 'example/static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'example/media')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "example/static"),
+)
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = '/static/'
+MEDIA_URL = '/media/'
+STATIC_URL = '/static/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
@@ -53,22 +59,23 @@ ADMIN_MEDIA_PREFIX = '/media/'
 SECRET_KEY = '11111999444444333344445555555588888000001111777777'
 
 # List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
+# TEMPLATE_LOADERS = (
+#    'django.template.loaders.filesystem.load_template_source',
+#    'django.template.loaders.app_directories.load_template_source',
 #     'django.template.loaders.eggs.load_template_source',
-)
+# )
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
 )
 
 ROOT_URLCONF = 'example.urls'
 
 TEMPLATE_CONTEXT_PROCESSORS = (
-    "django.core.context_processors.auth",
+    "django.contrib.auth.context_processors.auth",
     "django.core.context_processors.debug",
     "django.core.context_processors.i18n",
     "django.core.context_processors.media",
@@ -89,8 +96,12 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.admin',
     'django.contrib.humanize',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
     'hgwebproxy',
 )
 
-
-
+try:
+    from localsettings import *
+except ImportError:
+    pass
--- example/urls.py
+++ example/urls.py
@@ -1,17 +1,16 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import include, patterns, url 
 from django.contrib import admin
-from django.conf import settings
-from hgwebproxy import settings as hgwebproxy_settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+# from django.conf import settings
+
+# from hgwebproxy import settings as hgwebproxy_settings
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-    (r'^repo/', include('hgwebproxy.urls')),
+    url(r'^repo/', include('hgwebproxy.urls')),
 )
 
-if settings.DEBUG:
-    urlpatterns += patterns('',
-        (r'^static/(?P<path>.*)$', 'django.views.static.serve',
-            {'document_root': hgwebproxy_settings.STATIC_PATH}),
-    )
+urlpatterns += staticfiles_urlpatterns('/static/')
+
