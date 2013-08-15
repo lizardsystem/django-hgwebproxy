f47c8f0f2f4549510ea6ea90a4c07a1b2e1d89cc
Sander Smits <sander.smits@nelen-schuurmans.nl>
2013-08-15 15:37:56 +0200

    Simplify setup.py

--- setup.py
+++ setup.py
@@ -1,98 +1,47 @@
-# -*- coding:utf8 -*-
-from distutils.command.install_data import install_data
-from distutils.command.install import INSTALL_SCHEMES
-
 from setuptools import setup
 
-import os
-import sys
+version = '0.3.dev0'
 
-class osx_install_data(install_data):
-    # On MacOS, the platform-specific lib dir is /System/Library/Framework/Python/.../
-    # which is wrong. Python 2.5 supplied with MacOS 10.5 has an Apple-specific fix
-    # for this in distutils.command.install_data#306. It fixes install_lib but not
-    # install_data, which is why we roll our own install_data class.
+long_description = '\n\n'.join([
+    open('README.rst').read(),
+    open('CREDITS.rst').read(),
+    open('CHANGES.rst').read(),
+])
 
-    def finalize_options(self):
-        # By the time finalize_options is called, install.install_lib is set to the
-        # fixed directory, so we set the installdir to install_lib. The
-        # install_data class uses ('install_data', 'install_dir') instead.
-        self.set_undefined_options('install', ('install_lib', 'install_dir'))
-        install_data.finalize_options(self)
+install_requires = [
+    'Django',
+    'South',
+    'mercurial',
+],
 
-if sys.platform == "darwin":
-    cmdclasses = {'install_data': osx_install_data}
-else:
-    cmdclasses = {'install_data': install_data}
+tests_require = [
+    'nose',
+    'coverage',
+    'mock',
+    'factory_boy',
+]
 
-def fullsplit(path, result=None):
-    """
-    Split a pathname into components (the opposite of os.path.join) in a
-    platform-neutral way.
-    """
-    if result is None:
-        result = []
-    head, tail = os.path.split(path)
-    if head == '':
-        return [tail] + result
-    if head == path:
-        return result
-    return fullsplit(head, [tail] + result)
-
-# Tell distutils to put the data_files in platform-specific installation
-# locations. See here for an explanation:
-# http://groups.google.com/group/comp.lang.python/browse_thread/thread/35ec7b2fed36eaec/2105ee4d9e8042cb
-for scheme in INSTALL_SCHEMES.values():
-    scheme['data'] = scheme['purelib']
-
-# Compile the list of packages available, because distutils doesn't have
-# an easy way to do this.
-packages, data_files = [], []
-root_dir = os.path.dirname(__file__)
-if root_dir != '':
-    os.chdir(root_dir)
-hgwebproxy = 'hgwebproxy'
-
-for dirpath, dirnames, filenames in os.walk('hgwebproxy'):
-    # Ignore dirnames that start with '.'
-    for i, dirname in enumerate(dirnames):
-        if dirname.startswith('.'): del dirnames[i]
-    if '__init__.py' in filenames:
-        packages.append('.'.join(fullsplit(dirpath)))
-    elif filenames:
-        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
-
-# Small hack for working with bdist_wininst.
-# See http://mail.python.org/pipermail/distutils-sig/2004-August/004134.html
-if len(sys.argv) > 1 and sys.argv[1] == 'bdist_wininst':
-    for file_info in data_files:
-        file_info[0] = '\\PURELIB\\%s' % file_info[0]
-
-setup(
-    name='django-hgwebproxy',
-    version='0.2.0',
-    description='Django-hgwebproxy is an app to use and manage mercurial repositories within the django admin interface',
-    long_description=open('README.rst').read(),
-    author='Mario César Señoranis Ayala',
-    author_email='mariocesar.c50@gmail.com',
-    url='http://bitbucket.org/mariocesar/django-hgweproxy/',
-    packages = packages,
-    package_data = {'' : ['*.html', '*.txt']},
-    cmdclass = cmdclasses,
-    data_files = data_files,
-    license='GPL',
-    keywords='django mercurial hg bitbucket webapp',
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License (GPL)',
-        'Natural Language :: English',
-        'Natural Language :: Spanish',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-    ],
-    test_suite='tests.main',
-)
+setup(name='django-hgwebproxy',
+      version=version,
+      description=("django-hgwebproxy is a django application which provides "
+                   "a django version of mercurials hgwebdir interface."),
+      long_description=long_description,
+      # Get strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=['Programming Language :: Python',
+                   'Framework :: Django',
+                   ],
+      keywords=[],
+      author='Sander Smits',
+      author_email='sander.smits@nelen-schuurmans.nl',
+      url='',
+      license='GPL',
+      packages=['hgwebproxy'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=install_requires,
+      tests_require=tests_require,
+      extras_require={'test': tests_require},
+      entry_points={
+          'console_scripts': [
+          ]},
+      )
